6bb7b9a1af2223fe3578628804f35fb039910704
Alejandro Rios <alejandro.rios@bssltda.com>
2024-05-17 18:33:16 -0500

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "home",
+        loadComponent: () => import("./pages/home/home.component").then((m) => m.HomeComponent)
+    },
+    {
+        path: "auth",
+        loadChildren: () => import("./pages/auth/auth.routes").then((m) => m.AUTH_ROUTES)
+    },
+    {
+        path: "",
+        redirectTo: "/home",
+        pathMatch: "full"
+    }
+];
---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MainSectionComponent } from './main-section/main-section.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [MainSectionComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+}
