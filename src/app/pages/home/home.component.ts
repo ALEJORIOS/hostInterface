0f5b2ebe14d4c29b204f02a67a52f4ddaa9e091d
Alejandro Rios <alejandro.rios@bssltda.com>
2024-05-20 18:34:05 -0500

    style: add style to navbar

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MainSectionComponent } from './main-section/main-section.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MainSectionComponent],
+  imports: [MainSectionComponent, NavbarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
---
+++ src/app/pages/home/navbar/navbar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'navigation',
+  standalone: true,
+  imports: [],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
+})
+export class NavbarComponent {
+
+}
